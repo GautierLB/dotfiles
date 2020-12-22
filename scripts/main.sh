e48e27acf9b2f9abe5ab591dd1b13e936a679b2f
LE BISSONNAIS Gautier <gautier.lb@gmail.com>
2020-12-22 17:01:04 +0100

    feat: update powerlevel10k install folder (#2)
    Co-authored-by: Gautier Le Bissonnais <glebissonnais.externe@bedrockstreaming.com>

--- scripts/main.sh
+++ scripts/main.sh
@@ -2,13 +2,11 @@
 
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
-echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
-
-cp .* ~/
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/themes/powerlevel10k
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd "${DIR}/../"
+cp .* ~/
 
 # Install homebrew & dependencies
 ./scripts/homebrew.sh

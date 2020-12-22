2979ca4cd4b50f2d55f238dcdf5d8eef45cf742e
LE BISSONNAIS Gautier <gautier.lb@gmail.com>
2020-12-22 16:46:15 +0100

    feat: move main file and add ohmyzsh and powerlevel10k install (#1)
    Co-authored-by: Gautier Le Bissonnais <glebissonnais.externe@bedrockstreaming.com>

--- scripts/main.sh
+++ scripts/main.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
+
+cp .* ~/
+
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd "${DIR}/../"
 
